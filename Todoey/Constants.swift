42ff443738c7273522d4315fed288e22d8377992
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-23 14:02:40 -0700

    Add Todo items cell.

---
+++ Todoey/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Zimo Zhao on 4/23/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let TodoItemCellIdentifier = "TodoItemCell"
+}
