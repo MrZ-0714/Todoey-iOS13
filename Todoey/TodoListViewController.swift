febfc28218b99c3101f2d3bf1617447f87b418ec
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-04-23 15:44:08 -0700

    Test git commit in cmd.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -38,6 +38,26 @@ class TodoListViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: - Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+            
+        let alert  = UIAlertController(title: "Add new todo item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) { (action) in
+            print("Success")
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
